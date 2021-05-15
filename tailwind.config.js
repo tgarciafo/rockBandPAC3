e93ab2d1854949e6be7741561acdc1cfae11fd8a
tgarciafo <tgarciafo@uoc.edu>
2021-05-15 13:09:38 +0200

    home gairebé completa

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       'fons': '#fd9853',
       'nav': '#daf7a6'
       }),
+      borderColor: theme => ({
+        ...theme('colors'),
+        'light': '#daf7a6'
+      }),
+      width: {
+        '35':'35%'
+      },
+      borderWidth: {
+        '5':'2.5px'
+      },
       fontFamily: {
         panteres:['"New Rocker"', 'cursive']
       },
@@ -30,16 +40,46 @@ module.exports = {
         'xl': '30px',
         'xl2': '35px'
       },
+      height: {
+        'sm': '5px'
+      },
       gridTemplateColumns: {
         'portada': '1fr 3fr 1fr',
+        'portada-Desktop': 'repeat(2, .5fr) 45px repeat(3, .5fr)'
       },
       gridTemplateRows: {
         'portada': 'auto auto 50px auto 50px',
-      }
+        'portada-Desktop': 'auto auto 2fr .5fr'
+      },
+      textColor: theme => theme('colors'),
+     textColor: {
+       'play': '#fc1b35'
+     }
     }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-base-buttons')({
+      baseClass: '.button',
+      colors: {
+          theme: {
+              primary: {
+                  background: '#000',
+                  text: '#fff', 
+                  borderWidth: '1px',
+                  activeBackground: '#000',
+                  activeBorderWidth: '1px',
+                  activeBorderColor: '#fff',
+                  hoverBackground: '#d3cccc',
+                  hoverBorderColor: '#000',
+                  hoverBorderWidth: '1px',
+                  hoverText: '#fc1b35'
+              }
+          }
+      }
+  }),
+  require('tailwindcss-text-fill-stroke')()
+  ],
 }
